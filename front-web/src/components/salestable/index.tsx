2ff8e1ed81aa444d531c2c5835b3895050837117
Henrique Souza <henrique.asouza@outlook.com>
2022-01-08 15:00:39 -0300

    Add SalesTable component

---
+++ front-web/src/components/salestable/index.tsx
@@ -0,0 +1,80 @@
+import './styles.css';
+
+function SalesTable() {
+  return (
+    <div className="base-card sales-table-container">
+      <h3 className="sales-table-title">Vendas recentes</h3>
+      <table className="sales-table">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Data</th>
+            <th>Gênero</th>
+            <th>Categoria</th>
+            <th>Loja</th>
+            <th>Forma de pagamento</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#341</td>
+            <td>07/11/1991</td>
+            <td>Feminino</td>
+            <td>Roupas de acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/1991</td>
+            <td>Feminino</td>
+            <td>Roupas de acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/1991</td>
+            <td>Feminino</td>
+            <td>Roupas de acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/1991</td>
+            <td>Feminino</td>
+            <td>Roupas de acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/1991</td>
+            <td>Feminino</td>
+            <td>Roupas de acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+          <tr>
+            <td>#341</td>
+            <td>07/11/1991</td>
+            <td>Feminino</td>
+            <td>Roupas de acessorios</td>
+            <td>Uberlandia</td>
+            <td>Credito</td>
+            <td>R$ 540.000,00</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default SalesTable;
